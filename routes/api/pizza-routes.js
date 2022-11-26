bf84b98be986294e66fa03ef96523afbb50929a9
vpratnicki <vanessa@pratnicki.net>
2022-11-26 15:43:13 -0500

    Users can create, read, update, and delete data for a pizza's comment in the database

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -8,14 +8,12 @@ const {
   } = require('../../controllers/pizza-controller');
 
 // Set up GET all and POST at /api/pizzas
-router
-  .route('/')
+router.route('/')
   .get(getAllPizza)
   .post(createPizza);
 
 // Set up GET one, PUT, and DELETE at /api/pizzas/:id
-router
-  .route('/:id')
+router.route('/:id')
   .get(getPizzaById)
   .put(updatePizza)
   .delete(deletePizza);
